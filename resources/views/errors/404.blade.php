5b873b899679fd57f9298a1d8843a4799cf2a505
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-02-17 12:33:35 +0100

    added custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+    404 | Page not found
+@endsection
+
+@section('content')
+    <div class="row mt-3">
+        <div class="alert alert-danger text-center">
+            <h2 class="display-3">404</h2>
+            <p class="display-5">Page Not Found</p>
+        </div>
+    </div>
+@endsection
